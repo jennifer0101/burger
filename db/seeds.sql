99ac4c8af1e6b9730732945a66dedeb0ec3741db
Jennifer Zimmerman <jennifer.faye0101@gmail.com>
2019-10-12 18:40:25 -0500

    schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Impossible Burger');
+INSERT INTO burgers (burger_name) VALUES ('Black Bean Burger');
+INSERT INTO burgers (burger_name) VALUES ('Beyond Burger');
